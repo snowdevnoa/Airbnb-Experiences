933efbaeb49d131740776ea53f6324e5d0e2932c
xmasnoaa <snowdevnoa@gmail.com>
2023-02-03 21:52:37 -0600

    new images and activities added

--- src/data.js
+++ src/data.js
@@ -19,7 +19,7 @@ export default [
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
-    coverImg: './wedding-photography.png',
+    coverImg: './jeremy-wong-weddings-K8KiCHh4WU4-unsplash.jpg',
     stats: {
       rating: 5.0,
       reviewCount: 30,
@@ -33,7 +33,7 @@ export default [
     description:
       'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
     price: 50,
-    coverImg: './mountain-bike.png',
+    coverImg: './jake-schumacher-r92CDGlpMQE-unsplash.jpg',
     stats: {
       rating: 4.8,
       reviewCount: 2,
@@ -43,30 +43,58 @@ export default [
   },
   {
     id: 4,
-    title: 'Group Mountain Biking',
+    title: 'Surf Lessons',
     description:
-      'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
-    price: 50,
-    coverImg: './mountain-bike.png',
+      'Learn to surf in one of the most stunning beach destinations in the world with experienced instructors. (Board provided!)',
+    price: 60,
+    coverImg: './ranjith-alingal-4OoChCP72mE-unsplash.jpg',
     stats: {
-      rating: 4.8,
-      reviewCount: 2,
+      rating: 4.9,
+      reviewCount: 4,
     },
-    location: 'Norway',
-    openSpots: 3,
+    location: 'Bali, Indonesia',
+    openSpots: 5,
   },
   {
     id: 5,
-    title: 'Group Mountain Biking',
+    title: 'Yoga Retreat',
     description:
-      'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
-    price: 50,
-    coverImg: './mountain-bike.png',
+      'Escape from the city and immerse yourself in a weekend of yoga and mindfulness in a beautiful countryside setting.',
+    price: 75,
+    coverImg: './yannic-laderach-Dqx4XWuXu7w-unsplash.jpg',
     stats: {
-      rating: 4.8,
+      rating: 4.7,
+      reviewCount: 3,
+    },
+    location: 'Tuscany, Italy',
+    openSpots: 4,
+  },
+  {
+    id: 6,
+    title: 'Culinary Tour',
+    description:
+      'Discover the local cuisine and culture with a guided culinary tour through the streets of Paris. Sample delicious foods and learn from experienced chefs.',
+    price: 45,
+    coverImg: './daniel-roe-pXh99EfQqvE-unsplash.jpg',
+    stats: {
+      rating: 4.6,
       reviewCount: 2,
     },
-    location: 'Norway',
-    openSpots: 3,
+    location: 'Paris, France',
+    openSpots: 6,
+  },
+  {
+    id: 7,
+    title: 'Hiking Adventure',
+    description:
+      'Join a guided hiking tour through the majestic Swiss Alps and discover breathtaking mountain scenery and fresh air.',
+    price: 40,
+    coverImg: './xavier-von-erlach-Yk4Ru_Z_nNk-unsplash.jpg',
+    stats: {
+      rating: 4.8,
+      reviewCount: 5,
+    },
+    location: 'Swiss Alps, Switzerland',
+    openSpots: 2,
   },
 ];
